d1466257b9c44b78959ef54e41605734cf7a05f6
Özgün Levent Kaya <levent@codeway.co>
2022-12-14 17:58:27 +0300

    App Notification with Firebase

--- app/src/main/java/com/eae/busbarar/presentation/LoginActivity.kt
+++ app/src/main/java/com/eae/busbarar/presentation/LoginActivity.kt
@@ -1,35 +1,62 @@
-package com.eae.busbarar.presentation;
+package com.eae.busbarar.presentation
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.eae.busbarar.R
+import com.google.android.material.button.MaterialButton
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val username = findViewById<TextView>(R.id.username)
+        val password = findViewById<TextView>(R.id.password)
+        val loginbutton = findViewById<View>(R.id.loginbutton) as MaterialButton
+        loginbutton.setOnClickListener { view: View? ->
+            if (username.text.toString() == "admin" && password.text.toString() == "admin") {
+                startActivity(Intent(this@LoginActivity, OpenCameraActivity::class.java))
+                Toast.makeText(this@LoginActivity, "LOGIN SUCCESFULL", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(this@LoginActivity, "LOGIN FAILED", Toast.LENGTH_SHORT).show()
+        }
+        askNotificationPermission()
+    }
 
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
 
-import com.eae.busbarar.R;
-import com.google.android.material.button.MaterialButton;
-
-public class LoginActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        TextView username = findViewById(R.id.username);
-        TextView password = findViewById(R.id.password);
-
-        MaterialButton loginbutton = (MaterialButton) findViewById(R.id.loginbutton);
-
-        loginbutton.setOnClickListener(view -> {
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                startActivity(new Intent(LoginActivity.this, OpenCameraActivity.class));
-                Toast.makeText(LoginActivity.this,"LOGIN SUCCESFULL",Toast.LENGTH_SHORT).show();
-            }else
-                Toast.makeText(LoginActivity.this,"LOGIN FAILED",Toast.LENGTH_SHORT).show();
-        });
-
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(POST_NOTIFICATIONS)
+            }
+        }
     }
 }
