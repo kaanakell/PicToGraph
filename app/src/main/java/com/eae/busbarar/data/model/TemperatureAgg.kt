449a0b05ea7f102ac326c8a256b1a1b5fb2b07f7
Kaan <kaanakel@minddrawer.co>
2023-03-24 15:46:51 +0300

    Api Response Testing

---
+++ app/src/main/java/com/eae/busbarar/data/model/TemperatureAgg.kt
@@ -0,0 +1,15 @@
+package com.eae.busbarar.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class TemperatureAggResponse(
+    val datetime : String?,
+    val avg_temp: Float?,
+    val min_temp: Float?,
+    val max_temp: Float?,
+    val open_temp: Float?,
+    val close_temp: Float?,
+    val msg: String?
+) : Parcelable
