f9fa620217a478b563375a99013c9a5ca542ef8a
Kaan <kaanakel@minddrawer.co>
2023-04-18 18:53:10 +0300

    Swipe to delete button reveal test

--- app/src/main/java/com/eae/busbarar/presentation/ChartActivity.kt
+++ app/src/main/java/com/eae/busbarar/presentation/ChartActivity.kt
@@ -1,7 +1,9 @@
 package com.eae.busbarar.presentation
 
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
+import android.graphics.Canvas
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -157,7 +159,7 @@ class ChartActivity : AppCompatActivity(), ISensor {
     }
 
     private fun setRecyclerView() {
-        val simpleItemTouchCallback: ItemTouchHelper.SimpleCallback = object :
+        /*val simpleItemTouchCallback: ItemTouchHelper.SimpleCallback = object :
             ItemTouchHelper.SimpleCallback(
                 0,
                 ItemTouchHelper.RIGHT
@@ -183,7 +185,104 @@ class ChartActivity : AppCompatActivity(), ISensor {
             }
         }
         val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
-        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView)*/
+        ItemTouchHelper(object : ItemTouchHelper.Callback() {
+
+            private val limitScrollX = dipToPx(100f, this@ChartActivity)
+            private var currentScrollX = 0
+            private var currentScrollXWhenInActive = 0
+            private var initXWhenInActive = 0f
+            private var firstInActive = false
+
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                val dragFlags = 0
+                val swipeFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                return makeMovementFlags(dragFlags, swipeFlags)
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {}
+
+            override fun getSwipeThreshold(viewHolder: RecyclerView.ViewHolder): Float {
+                return Integer.MAX_VALUE.toFloat()
+            }
+
+            override fun getSwipeEscapeVelocity(defaultValue: Float): Float {
+                return Integer.MAX_VALUE.toFloat()
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+
+                    if (dX == 0f) {
+                        currentScrollX = viewHolder.itemView.scrollX
+                        firstInActive = true
+                    }
+
+                    if (isCurrentlyActive) {
+                        var scrollOffset = currentScrollX + (-dX).toInt()
+                        if (scrollOffset > limitScrollX) {
+                            scrollOffset = limitScrollX
+                        }
+                        else if (scrollOffset < 0) {
+                            scrollOffset = 0
+                        }
+
+                        viewHolder.itemView.scrollTo(scrollOffset, 0)
+                    }
+                    else {
+                        if (firstInActive) {
+                            firstInActive = false
+                            currentScrollXWhenInActive = viewHolder.itemView.scrollX
+                            initXWhenInActive = dX
+                        }
+
+                        if (viewHolder.itemView.scrollX < limitScrollX) {
+                            viewHolder.itemView.scrollTo((currentScrollXWhenInActive * dX / initXWhenInActive).toInt(), 0)
+                        }
+                    }
+                }
+            }
+
+            override fun clearView(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ) {
+                super.clearView(recyclerView, viewHolder)
+
+                if (viewHolder.itemView.scrollX > limitScrollX) {
+                    viewHolder.itemView.scrollTo(limitScrollX, 0)
+                }
+                else if (viewHolder.itemView.scrollX < 0) {
+                    viewHolder.itemView.scrollTo(0, 0)
+                }
+            }
+
+        }).apply {
+            attachToRecyclerView(binding.recyclerView)
+        }
+    }
+
+    private fun dipToPx(dipValue: Float, context: Context): Int {
+        return (dipValue * context.resources.displayMetrics.density).toInt()
     }
 
     private fun chartViewFullscreen() {
