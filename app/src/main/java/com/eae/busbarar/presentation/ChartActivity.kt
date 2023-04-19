2c492ddc63886cc07ce11e23075bb0cef749faa5
kaanakellMind <kaanakel@minddrawer.co>
2023-04-19 18:31:06 +0300

    Swipe to delete button is working

--- app/src/main/java/com/eae/busbarar/presentation/ChartActivity.kt
+++ app/src/main/java/com/eae/busbarar/presentation/ChartActivity.kt
@@ -1,11 +1,7 @@
 package com.eae.busbarar.presentation
 
-import android.content.ContentValues.TAG
-import android.content.Context
 import android.content.Intent
-import android.graphics.Canvas
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +9,9 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.eae.busbarar.Constants
 import com.eae.busbarar.R
 import com.eae.busbarar.data.model.EndDateTime
@@ -28,7 +25,6 @@ import com.github.aachartmodel.aainfographics.aatools.AAColor
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 @AndroidEntryPoint
@@ -48,8 +44,7 @@ class ChartActivity : AppCompatActivity(), ISensor {
     private var initialStartMargin: Int = 0
     private var initialTopMargin: Int = 0
     private var isFullscreen = false
-
-
+    private var toast: Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -112,8 +107,7 @@ class ChartActivity : AppCompatActivity(), ISensor {
                 true
             }
         }
-        setRecyclerView()
-        //chartOptions()
+        setUpRecyclerView()
         lineChartForDataObservation()
         hideSystemNavigationBars()
         drawEmptyCharts()
@@ -158,131 +152,47 @@ class ChartActivity : AppCompatActivity(), ISensor {
         viewModel.uploadSensorId(TextRecognitionRequest(item, aggvalue, Constants.startDateTime, Constants.endDateTime ))//TODO Api request with aggregation value and start/end date&time
     }
 
-    private fun setRecyclerView() {
-        /*val simpleItemTouchCallback: ItemTouchHelper.SimpleCallback = object :
-            ItemTouchHelper.SimpleCallback(
-                0,
-                ItemTouchHelper.RIGHT
-            ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                return false
+    private fun setUpRecyclerView() {
+        adapter.list = list
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val itemTouchHelper = ItemTouchHelper(object : SwipeHelper(binding.recyclerView) {
+            override fun instantiateUnderlayButton(position: Int): List<UnderlayButton> {
+                val deleteButton = deleteButton(position)
+                return listOf(deleteButton)
             }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
-                val position =  viewHolder.position
-                val temp = arrayListOf<String>()
-                list.forEachIndexed { index, s ->
-                    if(index != position) {
-                        temp.add(s)
-                    }
-                }
-                list = temp
-                adapter.list = list
-            }
-        }
-        val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
-        itemTouchHelper.attachToRecyclerView(binding.recyclerView)*/
-        ItemTouchHelper(object : ItemTouchHelper.Callback() {
-
-            private val limitScrollX = dipToPx(100f, this@ChartActivity)
-            private var currentScrollX = 0
-            private var currentScrollXWhenInActive = 0
-            private var initXWhenInActive = 0f
-            private var firstInActive = false
-
-            override fun getMovementFlags(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder
-            ): Int {
-                val dragFlags = 0
-                val swipeFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-                return makeMovementFlags(dragFlags, swipeFlags)
-            }
-
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                return true
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {}
-
-            override fun getSwipeThreshold(viewHolder: RecyclerView.ViewHolder): Float {
-                return Integer.MAX_VALUE.toFloat()
-            }
-
-            override fun getSwipeEscapeVelocity(defaultValue: Float): Float {
-                return Integer.MAX_VALUE.toFloat()
-            }
-
-            override fun onChildDraw(
-                c: Canvas,
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                dX: Float,
-                dY: Float,
-                actionState: Int,
-                isCurrentlyActive: Boolean
-            ) {
-                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
-
-                    if (dX == 0f) {
-                        currentScrollX = viewHolder.itemView.scrollX
-                        firstInActive = true
-                    }
-
-                    if (isCurrentlyActive) {
-                        var scrollOffset = currentScrollX + (-dX).toInt()
-                        if (scrollOffset > limitScrollX) {
-                            scrollOffset = limitScrollX
-                        }
-                        else if (scrollOffset < 0) {
-                            scrollOffset = 0
-                        }
-
-                        viewHolder.itemView.scrollTo(scrollOffset, 0)
-                    }
-                    else {
-                        if (firstInActive) {
-                            firstInActive = false
-                            currentScrollXWhenInActive = viewHolder.itemView.scrollX
-                            initXWhenInActive = dX
-                        }
-
-                        if (viewHolder.itemView.scrollX < limitScrollX) {
-                            viewHolder.itemView.scrollTo((currentScrollXWhenInActive * dX / initXWhenInActive).toInt(), 0)
-                        }
-                    }
-                }
-            }
-
-            override fun clearView(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder
-            ) {
-                super.clearView(recyclerView, viewHolder)
-
-                if (viewHolder.itemView.scrollX > limitScrollX) {
-                    viewHolder.itemView.scrollTo(limitScrollX, 0)
-                }
-                else if (viewHolder.itemView.scrollX < 0) {
-                    viewHolder.itemView.scrollTo(0, 0)
-                }
-            }
-
-        }).apply {
-            attachToRecyclerView(binding.recyclerView)
-        }
+        })
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
     }
 
-    private fun dipToPx(dipValue: Float, context: Context): Int {
-        return (dipValue * context.resources.displayMetrics.density).toInt()
+    private fun toast(text: String) {
+        toast?.cancel()
+        toast = Toast.makeText(this, text, Toast.LENGTH_SHORT)
+        toast?.show()
+    }
+
+    private fun deleteButton(position: Int) : SwipeHelper.UnderlayButton {
+        return SwipeHelper.UnderlayButton(
+            this,
+            "Delete",
+            14.0f,
+            android.R.color.holo_red_light,
+            object : SwipeHelper.UnderlayButtonClickListener {
+                override fun onClick() {
+                    val item = list[position]
+                    val position =  position
+                    val temp = arrayListOf<String>()
+                    list.forEachIndexed { index, s ->
+                        if(index != position) {
+                            temp.add(s)
+                        }
+                    }
+                    list = temp
+                    adapter.list = list
+                    toast("Deleted Sensor $item")
+                }
+            })
     }
 
     private fun chartViewFullscreen() {
