f2e315681ad31e0474f6ef4cbb6a0c99d0bcc82f
kaanakellMind <kaanakel@minddrawer.co>
2023-06-15 14:21:28 +0300

    Live Data Webview implemented, live data only shows the selected list items

--- app/src/main/java/com/eae/busbarar/Constants.kt
+++ app/src/main/java/com/eae/busbarar/Constants.kt
@@ -9,6 +9,7 @@ object Constants {
     const val BASE_URL = "http://10.45.50.32/"
     const val BASE_URL_WEB_CHART = "http://10.45.20.92/csrender"
     const val BASE_URL_ALERT_SCREEN = "10.45.50.32/alertwebview"
+    const val BASE_URL_LIVE_DATA = "10.45.50.32/livedata"
     const val USER_AGENT = "Thunder Client(https://www.thunderclient.com/)"
     const val startDateTime = "2023-05-10 11:15:00"
     const val endDateTime = "2022-11-21 13:30:00"
